63e64ab4b2c2fd749c4fe28f1bb84fd5f7159caa
Ruoxi Wang <38963059+westlike666@users.noreply.github.com>
2021-09-23 23:38:54 -0700

    Update ode_class.py

--- ode_class.py
+++ ode_class.py
@@ -14,8 +14,8 @@ from datetime import datetime
 
 from qutip import*
 
-N=2 # max number excitation
-L=4# number of site   
+N=2# max number excitation
+L=5# number of site   
 
 """
 for two-level system, the maxmum site the computer can handle for cnstructing initial matrix is about 20 
@@ -23,12 +23,13 @@ for two-level system, the maxmum site the computer can handle for cnstructing in
 for 1 billion entries it takes 10GB memory. so do not exceed 10^5 sites when using semi-classic
 
 """ 
-J=0.1#hopping 
+J=1#hopping 
 w=4+2*J #detunning 
-U=0 # onsite repulsion 
-A=0.1 #external driving 
-gamma=2#losse
+U=-10 # onsite repulsion 
+A=1 #external driving 
+gamma=1#losse
 
+pick=0 # pick the i th val
 
 """
 First using semi-classical approximation. 
@@ -37,10 +38,11 @@ start_time=datetime.now()
 
 model=Bose_Hubbard(N,L,w,U,J,A,gamma)
 ind_a, ind_adag_a, ind_a_a = model.get_index()
-y0=model.get_init_value()
+rho0=model.generate_random_density() # generate a random initial density every time called, stored as self.random_rho
+y0=model.get_random_value()
 #y0=np.random.randn(2*L**2+L)+(np.random.randn(2*L**2+L))*1j
 
-def f(t,Y):
+def f(t,Y): # This is a colsed set of coupled rate equations setting U=0:  equaiton 3 in PLA 91, 033823(2015)
     D=0*Y+0j
     
     for l in range(L):
@@ -68,37 +70,49 @@ def f(t,Y):
     return D   
 
 
-pick=2
 
-t_span=(0,10)
-t_eval=np.linspace(0, 10, 100)
-result=solve_ivp(f, y0=y0, t_span=t_span, t_eval=t_eval, vectorized=False)
+t_0=0
+t_1=10
+t_span=(t_0,t_1)
+t_eval=np.linspace(t_0, t_1, 100)
+
+
+
+result1=solve_ivp(f, y0=y0, t_span=t_span, t_eval=t_eval)
 
 end_time=datetime.now() 
 
 print("--- Duration for semiclassical with N={} levels and L={} sites is: {} ---\n" .format(N,L, end_time-start_time))
 
-plt.plot(result.t, result.y[pick])  
-
+plt.plot(result1.t, result1.y[pick], label='Semiclassical approx')  
 """
 Second using Qutip solver with full Lindblad equations 
 """
 start_time=datetime.now()
 
 H=model.get_Hamiltonian()
-rho0=model.generate_initial_density() 
 a_list, adag_list =model.generate_ladder_ops() #which is annihilation operators 
+adag_a_list=model.generate_adag_a_ops()
+a_a_list=model.generate_a_a_ops()
+
 c_ops=[]
 for i in range (L):
     c_ops.append(np.sqrt(gamma)*a_list[i])
     
 times=t_eval
 
-result2=mesolve(H, rho0, times, c_ops, e_ops=a_list)
+result2=mesolve(H, rho0, times, c_ops, e_ops=a_list+adag_a_list+a_a_list, progress_bar=True)    
+
 end_time=datetime.now()
 print("--- Duration for Qutip with N={} levels and L={} sites is: {} ---\n" .format(N,L, end_time-start_time))
 
-plt.plot(result2.times, result2.expect[pick]) 
+plt.plot(result2.times, result2.expect[pick], label='numerical solved Lindblad') 
+plt.title('Bose-Hubbard N=%d, L=%d ' % (N,L))
+plt.xlabel('t')
+plt.ylabel('$<a_{%d}>$' % pick)
+plt.legend()
+
+
 
 """Let us think about the scales before comparing the two reuslts !!"""
 
