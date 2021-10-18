2d5def3fa968879a2848da80eccdb02f7012f25f
Ruoxi Wang <38963059+westlike666@users.noreply.github.com>
2021-10-18 13:00:46 -0700

    Update XY_simu.py

--- XY_simu.py
+++ XY_simu.py
@@ -16,12 +16,17 @@ from tqdm import tqdm
 from qutip import*
 import random
 from Lindblad_solver import Lindblad_solve
-
+from energy_paras import Energycomputer, Jcomputer, Ucomputer, Gammacomputer
 
 
 L=2
-N=4
+N=3
 
+W=1
+t=0.1
+u=0
+
+seed=1
 
 show_type='z'
 #show_ind=random.randrange(N)
@@ -29,14 +34,23 @@ show_ind=0
 
 model=XY(L,N)
 
-H=model.get_Hamiltonian(W=1, t=0.1, u=0)
-#rho0=model.generate_coherent_density()
-rho0=model.generate_random_density(seed=1)
+
+eps=Energycomputer(N,seed).uniformrandom_e(W)
+J=Jcomputer(N, nn_only=False, scaled=False, seed=seed).uniformrandom_j(t)
+U=Ucomputer(N, nn_only=False, scaled=True, seed=seed).uniformrandom_u(u)
+
+#G_comp=Gammacomputer(N).constant_g(G)    
+
+H=model.get_Hamiltonian2(eps, J, U)
+
+rho0=model.generate_coherent_density(alpha=1*np.pi/2.5)
+#rho0=model.generate_random_density(seed=1)
+
 #print(rho0)
 Sz=model.Sz
 Sp=model.Sp
 Sm=model.Sm
-#G=0.0000000000000001
+#G=0.000000000000000
 G=0.1
 
 gamma=model.generate_gamma(G) # if gamma is too large will cause too stiff ode, thus need to increase number of steps correspondingly.
@@ -45,9 +59,6 @@ gamma=model.generate_gamma(G) # if gamma is too large will cause too stiff ode, 
 """
 sloving by semi-classical 1st order: <S1*S2>=<S1>*<S2>
 """
-eps=model.eps
-J=model.J
-U=model.U
 
 y0=expect(model.Sz+model.Sp, rho0)
    
@@ -61,14 +72,15 @@ fun=ode_funs.fun_1st
 index=ode_funs.flat_index(single_ops=['z','+'], double_ops=[], index={}) 
 
 t_0=0
-t_1=20
+t_1=100
 t_span=(t_0,t_1)
 t_eval=np.linspace(t_0, t_1, 1000 )
 
 
-result1=solve_ivp(fun, t_span=t_span, y0=y0, t_eval=t_eval, args=[index])  
+with tqdm(total=100, unit="‰") as pbar:
+    result1=solve_ivp(fun, t_span=t_span, y0=y0, t_eval=t_eval, args=[index,pbar, [t_0, (t_1-t_0)/100]])  
 
-plt.plot(result1.t, result1.y[index[show_type][show_ind]], label='1st-order approx')
+
 
 
 
@@ -99,20 +111,27 @@ ops=Options(tidy=(False), average_expect=(False))
     
 result2=mesolve(H, rho0, times, c_ops, e_ops, progress_bar=True, options=None) 
 
-plt.plot(result2.times, result2.expect[index[show_type][show_ind]], label='Qutip solved Lindblad') 
 
 
-result3, expect_value=Lindblad_solve(H, rho0, t_span, t_eval, c_ops=c_ops, e_ops=e_ops)  
+# result3, expect_value=Lindblad_solve(H, rho0, t_span, t_eval, c_ops=c_ops, e_ops=e_ops)  
 
-plt.plot(result3.t, expect_value[index[show_type][show_ind]], label='solve_ivp solved Lindblad') 
+# plt.plot(result3.t, expect_value[index[show_type][show_ind]], label='solve_ivp solved Lindblad') 
 
-
-
+plt.figure()
+plt.plot(result1.t, result1.y[index[show_type][show_ind]].real, label='1st-order approx')
+plt.plot(result2.times, result2.expect[index[show_type][show_ind]].real, label='Qutip solved Lindblad') 
 plt.title('XY model L=%d, N=%d  gamma=%dW for ' % (L,N,G)+Diss)
 plt.xlabel('t')
 plt.ylabel("$<S^{}_{}>$".format(show_type, show_ind))
 plt.legend()  
 
+plt.figure()
+plt.plot(result1.t, result1.y[index[show_type][show_ind]].imag, label='1st-order approx')
+plt.plot(result2.times, result2.expect[index[show_type][show_ind]].imag, label='Qutip solved Lindblad') 
+plt.title('XY model L=%d, N=%d  gamma=%dW for ' % (L,N,G)+Diss)
+plt.xlabel('t')
+plt.ylabel("$<S^{}_{}>^*$".format(show_type, show_ind))
+plt.legend()  
 
 
 
