a050e6cfe158378abc1938c384a52f7f52648ebb
Ruoxi Wang <38963059+westlike666@users.noreply.github.com>
2021-11-02 17:33:57 -0700

    Update XY_run_Qutip.py

--- XY_run_Qutip.py
+++ XY_run_Qutip.py
@@ -43,16 +43,16 @@ show_ind=0
 model=XY(L,N)
 
 
-eps=Energycomputer(N,seed).constant_e(W)
-J=Jcomputer(N, nn_only=False, scaled=True, seed=seed).constant_j(t)
+eps=Energycomputer(N,seed).uniformrandom_e(W)
+J=Jcomputer(N, nn_only=False, scaled=False, seed=seed).uniformrandom_j(t)
 U=Ucomputer(N, nn_only=False, scaled=True, seed=seed).uniformrandom_u(u)
 gamma=Gammacomputer(N).central_g(G)
 #gamma=Gammacomputer(N).boundary_g(G)
-#gamma=Gammacomputer(N).site_g(G,[2,3,4])
+#gamma=Gammacomputer(N).site_g(G,[0,6])
 
 H=model.get_Hamiltonian2(eps, J, U)
 
-#states,rho0=model.generate_coherent_density(alpha=1*np.pi/4)
+#states,rho0=model.generate_coherent_density(alpha=1*np.pi/2.1)
 #states,rho0=model.generate_random_density(seed=None, pure=True) #seed works for mixed state
 states,rho0=model.generate_random_ket(seed=None)
 print(states)
@@ -142,7 +142,7 @@ def plot_evolution(show_type='z', show_ind=0):
     # plt.ylabel("$Im <S^{}_{}>$".format(show_type, show_ind))
     # plt.legend()
     plt.xlabel('t')
-    plt.suptitle('XY model L=%d, N=%d  eps=%.2f  t=%.1f W  g=%.1f W from one end' % (L,N, eps[show_ind],t,G))
+    plt.suptitle('XY model L=%d, N=%d  eps=%.2f  t=%.2f W  g=%.1f W from one end' % (L,N, eps[show_ind],t,G))
 for show_ind in range(N):
     plot_evolution('+',show_ind)
     plot_evolution('z', show_ind)
