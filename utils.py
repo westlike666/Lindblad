05f1c15ec7d11015d4c6583990f97588c72d4310
Ruoxi Wang <38963059+westlike666@users.noreply.github.com>
2021-11-04 20:08:19 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,10 +6,24 @@ Created on Mon Oct 25 17:27:10 2021
 """
 
 import datetime as datetime
+import pickle
 
 
 def get_run_time():
     currentDT = datetime.datetime.now()
     return "%d-%d-%d-%d_%d_%d" % (currentDT.year, currentDT.month, currentDT.day, currentDT.hour, currentDT.minute, currentDT.second)
+
+def store_vars(path, obj):
+    f=open(path+'\\store.p', 'wb')
+    pickle.dump(obj, f)
+    f.close()
+    
+def load_vars(path):
+    f=open(path, 'rb')
+    return pickle.load(f)
+    
+    
+    
+    
 
 
