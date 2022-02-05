ab06a8031098313f9d0950b7d477298369ebcbf0
Ruoxi Wang <38963059+westlike666@users.noreply.github.com>
2022-02-04 18:50:13 -0800

    Create parallel_job.sh

---
+++ parallel_job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --time=2:00:00
+#SBATCH --account=def-edgrant
+#SBATCH --job-name=results/job
+#SBATCH --output=%x-%j.out
+#SBATCH --cpus-per-task=16
+#SBATCH --nodes=4
+#SBATCH --mem=128G 
+
+module load python scipy-stack
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+
+pip install --no-index --upgrade pip
+pip install qutip --no-deps 
+pip install tqdm
+
+python XY_parallel.py --N 5 --W 0 --t 0 --G 0 
