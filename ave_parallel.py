954de9b88e5fbc74a21a21efe05bed4920b4f17f
Ruoxi Wang <38963059+westlike666@users.noreply.github.com>
2022-02-13 17:23:52 -0800

    Update ave_parallel.py

--- ave_parallel.py
+++ ave_parallel.py
@@ -21,14 +21,14 @@ G=0.5
 #seed=1
 
 save=True
-path0='results/boundary/'
+path0='results/center/'
 
 
 
 for i in range(N):
     Sz_total=[]
     Sp_total=[]
-    for seed in range(1,17):          
+    for seed in range(5,17):          
         name='N='+str(N)+' W='+ str(W)+' t='+str(t) + ' g='+ str(G) +' seed=' +str(seed)
         path=path0 +name 
         data=utils.load_vars(path+'/store.p')
