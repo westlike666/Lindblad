c4edb4018d24c4ed095346340319a3c65ed62336
Ruoxi Wang <38963059+westlike666@users.noreply.github.com>
2022-02-18 18:56:34 -0800

    Update ave_parallel.py

--- ave_parallel.py
+++ ave_parallel.py
@@ -11,55 +11,55 @@ import utils
 import matplotlib.pyplot as plt
 #from qutip import*
 
-Data=[]
-Ave=[]
+Data = []
+Ave = []
 
-N=11
-W=10.0
-t=1.0
-G=1.0
-#seed=1
+N = 11
+W = 0.0
+t = 1.0
+G = 0.5
+# seed=1
 
-save=True
-path0='results/center/'
-
+save = True
+path0 = 'results/center/'
 
 
 for i in range(N):
-    Sz_total=[]
-    Sp_total=[]
-    for seed in range(5,21):          
-        name='N='+str(N)+' W='+ str(W)+' t='+str(t) + ' g='+ str(G) +' seed=' +str(seed)
-        path=path0 +name +'/store.p'
+    Sz_total = []
+    Sp_total = []
+    for seed in range(1, 21):
+        name = 'N='+str(N)+' W=' + str(W)+' t='+str(t) + \
+            ' g=' + str(G) + ' seed=' + str(seed)
+        path = path0 + name + '/store.p'
         if os.path.exists(path):
-            data=utils.load_vars(path)
-    
-            eps=data['eps']
-            y1=data['y1']
-            y2=data['y2']
-            index=data['index'] 
-            t1=data['t1']
-            t2=data['t2']
-            
+            data = utils.load_vars(path)
+
+            eps = data['eps']
+            y1 = data['y1']
+            y2 = data['y2']
+            index = data['index']
+            t1 = data['t1']
+            t2 = data['t2']
+
     #        S_z=[eps[i]*y1[i] for i in index['z']]
-            t_total=np.append(t1, t2)
+            t_total = np.append(t1, t2)
             Sz_total.append(np.append(y1[index['z'][i]], y2[index['z'][i]]))
             Sp_total.append(np.append(y1[index['+'][i]], y2[index['+'][i]]))
-            
-    Sz_ave=(np.mean(Sz_total,0)) 
-    Sp_ave=(np.mean(Sp_total,0))
+
+    Sz_ave = (np.mean(Sz_total, 0))
+    Sp_ave = (np.mean(Sp_total, 0))
     plt.figure()
-    plt.plot(t_total*t,Sz_ave, label="$Re <S^z_{%d} $" % (i))
-    plt.plot(t_total*t,Sp_ave, label="$Re <S^+_{%d} $" % (i))
+    plt.plot(t_total*t, Sp_ave, label="$Re <S^+_{%d} $" % (i))    
+    plt.plot(t_total*t, Sz_ave, label="$Re <S^z_{%d} $" % (i))
+
     plt.legend()
     plt.ylabel("site {}".format(i))
-    plt.ylim(-0.5,0.5)
+    plt.ylim(-0.5, 0.5)
     plt.axhline(y=0, color='grey', linestyle='--')
-    plt.xlabel('t * $\overline{J}$')
-    plt.suptitle('XY model, N=%d, W=%d,  $\overline{J}$=%.2f  g=%.1f' % (N,W,t,G))
+#    plt.xlabel('t * $\overline{J}$')
+    plt.suptitle(
+        'XY model, N=%d, W=%d,  $\overline{J}$=%.2f  g=%.1f' % (N, W, t, G))
     if save:
-       plt.savefig(path0+"/site {}.png".format(i))  
-    
-    
-    
-    
+        savename =path0+ 'Ave '+'N='+str(N)+' W=' + str(W)+' t='+str(t) +' g=' + str(G)
+        os.makedirs(savename, exist_ok=True)
+        plt.savefig(savename +"/site {}.png".format(i))
