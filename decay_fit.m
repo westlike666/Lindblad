d916d38f066c1b38fbe3fc465253976ddfcc6858
Ruoxi Wang <38963059+westlike666@users.noreply.github.com>
2023-04-13 19:15:53 -0700

    decays

--- decay_fit.m
+++ decay_fit.m
@@ -1,10 +1,15 @@
+
 close all
+load('decays.mat')
+Y=Y_bd;
+Gamma=[0.1 0.5 1 5 10];
+
+
 % Define the power-law function to fit the data
 powerlaw = @(a, b, x) a * x.^b;
 
 % Sample data (replace with your own data)
-x = [1:11]'
-;
+x = [1:11]';
 for i=[1:5]
 y = Y(1:end,i)    
 % Define lower and upper bounds for the fitting parameters
@@ -27,12 +32,19 @@ y_fit = powerlaw(a, b, x_fit);
 
 % Plot the original data and the fitted curve
 %figure;
-scatter(x, y, 'blue', 'DisplayName', 'Original data');
+h=plot(x, y, '.','DisplayName', ['\gamma_0=' num2str(Gamma(i)) ],'markersize',20);
+c=get(h,'color')
 hold on;
-plot(x_fit, y_fit, 'red', 'DisplayName', sprintf('Fitted power-law curve: y=%.4fx^{%.4f}', a, b));
-xlabel('x');
-ylabel('y');
-set()
-legend;
+plot(x_fit, y_fit,'--','color',c,'linewidth',1, 'DisplayName', sprintf('y=%.2fx^{%.1f}', a, b));
+xlabel('distance');
+ylabel('decay rate');
+%set(gca,'Xscale','log')
+set(gca,'YScale','log')
+legend('location','bestoutside');
+legend box off
 %hold off;
+set(gca,'FontSize',12)
+box off
 end
+
+print(gcf, '-dpdf', 'decay_curves.pdf');
